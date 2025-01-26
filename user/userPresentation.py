543874045c8b21c46ef69f122344b238627aa790
Thomas <tlemaire1610@gmail.com>
2025-01-26 23:08:53 +0100

    add logs for get user

--- user/userPresentation.py
+++ user/userPresentation.py
@@ -2,10 +2,15 @@
 from fastapi import APIRouter
 
 from user.userRepository import UserRepository
+from infrastructure.logging.logging_config import logger
+from user.userSchema import UserFlat
+
 
 
 router = APIRouter(prefix="/user")
 
 @router.get("")
 async def get_user(username: str):
-    return await UserRepository().get_by_username(username)
+    user: UserFlat =  await UserRepository().get_by_username(username)
+    logger.info("Utilisateur trouvé : %s", user)
+    return user
