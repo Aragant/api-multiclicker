e73a3d5a2c18d4d9fe4b8f1de5f61e9b69c134a7
Thomas <tlemaire1610@gmail.com>
2025-01-26 23:08:39 +0100

    add databaseerror

--- infrastructure/error/exeption_handler.py
+++ infrastructure/error/exeption_handler.py
@@ -2,10 +2,14 @@
 
 from fastapi import Request
 from fastapi.responses import JSONResponse
-from infrastructure.error.error import NotFoundError
+from infrastructure.error.error import DatabaseError, NotFoundError
 
 
 def setup_exepction_handlers(app):
     @app.exception_handler(NotFoundError)
     async def not_found_handler(request: Request, exc: NotFoundError):
         return JSONResponse(content={"NotFoundError: ": f"{exc.ressource} not found"}, status_code=404)
+    
+    @app.exception_handler(DatabaseError)
+    async def database_error_handler(request: Request, exc: DatabaseError):
+        return JSONResponse(content={"DatabaseError": "Database error"}, status_code=500)
