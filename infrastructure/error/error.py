69d322af349a56500c8e8f17de66768a43d41088
Thomas <tlemaire1610@gmail.com>
2025-01-28 21:25:09 +0100

    ♻️ Refactor NotFoundError and improve error handling in DB queries

--- infrastructure/error/error.py
+++ infrastructure/error/error.py
@@ -2,7 +2,7 @@ from fastapi.responses import JSONResponse
 
 
 class NotFoundError(Exception):
-    def __init__(self, ressource: str):
+    def __init__(self, ressource: list[str]):
         self.ressource = ressource
 
 class DatabaseError(Exception):
