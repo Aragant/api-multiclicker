cb690b39792dfffb8e61b0ec420f20654e91b09d
Thomas <tlemaire1610@gmail.com>
2025-01-28 19:40:00 +0100

    ➖  remove passlib depreciate

--- infrastructure/security/password_hash_service.py
+++ infrastructure/security/password_hash_service.py
@@ -1,3 +1,4 @@
+import bcrypt
 from passlib.context import CryptContext
 
 
@@ -5,8 +6,22 @@ from passlib.context import CryptContext
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
+# def verify_password(plain_password, hashed_password):
+#     return pwd_context.verify(plain_password, hashed_password)
 
-def get_password_hash(password):
-    return pwd_context.hash(password)
+# def get_password_hash(password):
+#     return pwd_context.hash(password)
+
+
+def to_bits(*args):
+  return tuple(s.encode('utf-8') for s in args)
+
+def to_str(*args):
+  return tuple(b.decode('utf-8') for b in args)
+
+def get_password_hash(pwd: str) -> str:
+  salt = bcrypt.gensalt()
+  return to_str(bcrypt.hashpw(*to_bits(pwd), salt))[0]
+
+def verify_password(plain_pwd: str, hashed_pwd: str) -> bool:
+  return bcrypt.checkpw(*to_bits(plain_pwd, hashed_pwd))
