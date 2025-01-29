1d916dc823ecf2dc968ff5108ffaaadb72a524fc
Thomas <tlemaire1610@gmail.com>
2025-01-29 22:17:00 +0100

    ✨ Start to added refresh token support and expiration logic

--- domain/auth/auth_presentation.py
+++ domain/auth/auth_presentation.py
@@ -4,15 +4,17 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
 from pydantic import BaseModel
 
-from domain.auth.authentication_service import authenticate_user, create_access_token, get_current_active_user
+from domain.auth.authentication_service import authenticate_user, create_access_token, create_refresh_token, get_current_active_user
 from domain.user.user_schema import UserPrivate
 
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
+REFRESH_TOKEN_EXPIRE_DAYS = 7
 
 class Token(BaseModel):
     access_token: str
+    refresh_token: str
     token_type: str
 
 
@@ -31,7 +33,14 @@ async def login_for_access_token(
     access_token = create_access_token(
         data={"sub": user.username}, expires_delta=access_token_expires
     )
-    return Token(access_token=access_token, token_type="bearer")
+    
+    refresh_token = create_refresh_token(
+        data={"sub": user.username}, expires_delta=timedelta(days=REFRESH_TOKEN_EXPIRE_DAYS)
+    )
+    
+    #implementer la sauvegarde du refresh token
+    
+    return Token(access_token=access_token, refresh_token=refresh_token, token_type="bearer")
 
 
 
--- domain/auth/authentication_service.py
+++ domain/auth/authentication_service.py
@@ -18,7 +18,7 @@ from infrastructure.logging.logging_config import logger
 
 ALGORITHM = "HS256"
 SECRET_KEY = os.environ.get("SECRET_KEY")
-
+SECRET_REFRESH_KEY = os.environ.get("SECRET_REFRESH_KEY")
 
 
 
@@ -43,6 +43,16 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
         expire = datetime.now(timezone.utc) + timedelta(minutes=15)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+def create_refresh_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(days=7)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_REFRESH_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
 
