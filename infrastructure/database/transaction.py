12d984eac59935a8b5720d909b633e48bdadd061
Thomas <tlemaire1610@gmail.com>
2025-01-31 23:52:27 +0100

    ⚰️ Delete auto-commit in session.

--- infrastructure/database/transaction.py
+++ infrastructure/database/transaction.py
@@ -16,7 +16,6 @@ async def transaction():
     session: AsyncSession = async_session()
     try:
         yield session
-        await session.commit()
     except Exception as e:
         await session.rollback()
         database_error_logger(e)
