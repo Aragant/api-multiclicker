10e9acb63900cc87b1d53e75e359446db41abdbd
huggotrico <hauretax@gmail.com>
2025-03-29 01:00:57 +0100

    guild model et user model n arrive pas a linker ensemble

--- domain/guild/guild_model.py
+++ domain/guild/guild_model.py
@@ -1,21 +1,20 @@
 import uuid
+from typing import List
 from sqlalchemy import Boolean, Column, DateTime, String, ForeignKey
 from sqlalchemy.sql import func
-from sqlalchemy.orm import relationship, Mapped
+from sqlalchemy.orm import relationship, Mapped, mapped_column
 from infrastructure.database.database import Base
-from domain.user.user_model import User
 
 class Guild(Base):
     __tablename__ = "guild"
-    id = Column(String, primary_key=True, index=True, default=lambda: str(uuid.uuid4()))
-    owner_id = Column(String, ForeignKey(User.id), nullable=False)
+    id: Mapped[str] = mapped_column(String, primary_key=True, index=True, default=lambda: str(uuid.uuid4()))
     name = Column(String, unique=True, nullable=True)
     description = Column(String, nullable=True)
     disable = Column(Boolean, default=False)
     create_date = Column(DateTime, default=func.now())
-     
-    user : Mapped["User"] = relationship("User", back_populates="guild")
-     
+    guild_members: Mapped[List["User"]] = relationship("User", foreign_keys="[User.my_guild_id]")
+    owner: Mapped["User"] = relationship("User", back_populates="ownered_guild", foreign_keys="[User.ownered_guild_id]")
+
     @property
     def as_dict(self):
         return {c.name: getattr(self, c.name) for c in self.__table__.columns}
--- domain/user/user_model.py
+++ domain/user/user_model.py
@@ -1,12 +1,12 @@
 import uuid
-from sqlalchemy import Boolean, Column, DateTime, Integer, String, UniqueConstraint
+from sqlalchemy import Boolean, Column, DateTime, String, UniqueConstraint, ForeignKey
 from sqlalchemy.sql import func
-from sqlalchemy.orm import relationship, Mapped
+from sqlalchemy.orm import relationship, Mapped, mapped_column
 from infrastructure.database.database import Base
 
 class User(Base):
     __tablename__ = "users"
-    id = Column(String, primary_key=True, index=True, default=lambda: str(uuid.uuid4()))
+    id: Mapped[str] = mapped_column(String, primary_key=True, index=True, default=lambda: str(uuid.uuid4()))
     username = Column(String, unique=True, nullable=True)
     password = Column(String, nullable=True)
     provider = Column(String, default="local", nullable=True)
@@ -14,9 +14,12 @@ class User(Base):
     description = Column(String, nullable=True)
     disabled = Column(Boolean, default=False)
     register_date = Column(DateTime, default=func.now())
+    my_guild_id: Mapped[str] = mapped_column(ForeignKey("guild.id"), nullable=True)
+    ownered_guild_id: Mapped[str] = mapped_column(ForeignKey("guild.id"), nullable=True)
     __table_args__ = (UniqueConstraint('email', 'provider', name='unique_email_per_provider'),)
 
-    guild: Mapped["Guild"] = relationship("Guild", back_populates="user")
+    ownered_guild: Mapped["Guild"] = relationship("Guild", back_populates="owner", foreign_keys=[ownered_guild_id])
+    my_guild: Mapped["Guild"] = relationship("Guild", foreign_keys=[my_guild_id])
 
     @property
     def as_dict(self):
