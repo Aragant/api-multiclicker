00fe87820eeaf81a008bc3726bf771632bab5f0c
Thomas <tlemaire1610@gmail.com>
2025-04-01 10:40:56 +0200

    ♻️ Add test cases for base repository get method and update .gitignore

--- test/test_base_repository/test_get.py
+++ test/test_base_repository/test_get.py
@@ -11,7 +11,7 @@ async def repo(async_session):
 
 
 @pytest.mark.asyncio
-async def test_get(async_session, repo):
+async def test_get_when_the_data_exists_should_return_it(async_session, repo):
     """Teste la récupération d'un élément dans la base de données."""
     async with async_session as session:
         session.add(FakeModel(id=1, username="test"))
@@ -20,3 +20,9 @@ async def test_get(async_session, repo):
     result = await repo._get(id=1)
     assert result.get("id") == 1
     assert result.get("username") == "test"
+    
+    
+@pytest.mark.asyncio
+async def test_get_when_the_data_does_not_exist_should_return_none(async_session, repo):
+    result = await repo._get(id=1)
+    assert result is None
