bcb6badab2ebb47bd72c6553756dfd1719edbd25
Thomas <tlemaire1610@gmail.com>
2025-04-03 17:48:37 +0200

    Enhance signup usecase with added DuplicateEntryError

--- infrastructure/error/error.py
+++ infrastructure/error/error.py
@@ -8,3 +8,7 @@ class NotFoundError(Exception):
 class DatabaseError(Exception):
     def __init__(self):
         pass
+
+class DuplicateEntryError(Exception):
+    def __init__(self, message: str):
+        super().__init__(message)
