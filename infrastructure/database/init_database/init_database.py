3b429116831ccfe82d68b9a69871ffabbc26fe79
huggotrico <hauretax@gmail.com>
2025-04-06 15:59:25 +0200

    orrection of miss merging init_database

--- infrastructure/database/init_database/init_database.py
+++ infrastructure/database/init_database/init_database.py
@@ -1,18 +1,17 @@
+from domain.user.user_service import UserService
+from domain.user.user_schema import UserSignUp, UserPrivate
 from domain.guild.guild_domain import GuildDomain
 from domain.guild.guild_schema import GuildCreateRequestBody
-from domain.user.user_domain import UserDomain
-from domain.user.user_service import UserService
-from domain.user.user_schema import UserSignUp
+
 
 async def init_database():
-    await create_main_user()
-
-async def create_main_user():
-    user: UserSignUp = UserSignUp(username="admin", password="admin", email="admin")
-    admin = await UserDomain().create(user)
+    admin = await create_main_user()
     guild = await GuildDomain().create(
         GuildCreateRequestBody(name="guild1", description="admin"), admin.id
     )
     print(f"guild id is {guild.id}")
-    await UserService().create(user)
 
+
+async def create_main_user() -> UserPrivate:
+    user: UserSignUp = UserSignUp(username="admin", password="admin", email="admin")
+    return await UserService().create(user)
