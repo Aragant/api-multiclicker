dd188b641095187aa580e583a5876a0bc2435107
Thomas <tlemaire1610@gmail.com>
2025-04-14 23:01:06 +0200

    ♻️ Remove unused imports

--- domain/guild/use_cases/create_guild.py
+++ domain/guild/use_cases/create_guild.py
@@ -1,11 +1,9 @@
-from sqlite3 import IntegrityError as SQLiteIntegrityError
-from sqlalchemy.exc import IntegrityError as SQLAlchemyIntegrityError
 from domain.guild.guild_model import Guild
 from domain.guild.guild_repository import GuildRepository
 from domain.guild.guild_schema import GuildCreateRequestBody, GuildFlat
 from domain.guild.guild_services import GuildServices
 from domain.member.member_model import Member, MemberRole
-from infrastructure.error.error import ConflictError, DuplicateEntryError
+from infrastructure.error.error import DuplicateEntryError
 
 
 async def create_guild(newGuild: GuildCreateRequestBody, user_id: str) -> GuildFlat:
@@ -18,5 +16,6 @@ async def create_guild(newGuild: GuildCreateRequestBody, user_id: str) -> GuildF
         description=newGuild.description,
         members=[Member(user_id=user_id, role=MemberRole.MASTER.value)],
     )
+    
     created_guild = await GuildRepository().save(guild)
     return GuildFlat.model_validate(created_guild)
